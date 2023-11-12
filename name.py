2b8c5ccc20fd8d6b6a3848c6a21461576509f31e
JusTyFeL <grikoanton5@gmail.com>
2023-11-12 21:21:00 +0300

    Create name.py

---
+++ name.py
@@ -0,0 +1,40 @@
+
+#Задача D
+# text = "Hello, world!"
+
+# print(text.replace("Hello, world!","!world, Hello"))
+
+#Задача E
+# text =  'Hold Cofffeee'
+# ind = text.find('f')
+# inds = text.rfind('f')
+# if ind != -1 and inds != -1:
+#     print('Первый индекс>>',ind )
+#     print('Второй индекс>>',inds)
+# elif ind == -1:
+#         print("Хухы>> ", ind)
+
+# задача f
+# text =  'Hold cceee'
+# inder = text.find('f')
+# if inder != -1:
+#         print('-1')
+# else:
+#         print('-2')
+
+#задача g
+# text = 'ats Very good bukva h and burva hh top!'
+
+# ind = text.find('h')
+# inds = text.rfind('h')
+
+# print(text[:ind] + text[inds+1: ])
+
+#задача x
+
+# text = 'Im ti loy bukva h nushna dla perevorachirovania h bukva zdox '
+
+# ind = text.find('h')
+# inds = text.rfind('h')
+
+# print(text[ind:], text[::inds - 1])
